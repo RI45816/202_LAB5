4a3bde30506f43c10a460efa7846e5a053b363ca
RI45816 <uu3@umbc.edu>
2017-03-08 19:53:43 -0500

    NH0Z2

--- lab5.cpp
+++ lab5.cpp
@@ -1,34 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*****************************************
- ** File:    lab5.cpp
- ** Project: CMSC 202 lab5, Spring 2017
- ** Author:  Uzoma Uwanamodo
- ** Date:    00/08/2017
- ** Section: 05
- ** E-mail:  uu3@umbc.edu
- ** 
- ** This file contains the main driver program for lab5.
- ** 
- ** 
- ** 
- ** 
- *********************************************/
-
-#include <cstdlib>
+#include "File.h"
+#include <string>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+void populateDirectory(vector<File>&);
+// fill vector - fills file info
+// @param vector<File>& - files in directory
+void printDirectory(vector<File>&);
+//printDirectory - prints the information of the files in the directory
+//@param const vector<File>& files in directory 
 
-    return 0;
+int main () {
+	//Creates a new directory of files
+  vector<File> myDirectory;
+	//Populates the vector
+  populateDirectory(myDirectory);
+  //Prints the contents of the vector
+  printDirectory(myDirectory);
+  return 0;
 }
 
+void populateDirectory(vector<File>& newDirectory) {
+  
+  //Write this function
+  //Needs to ask how many files
+  //Gets file name, file type, and file size
+  //Uses the overloaded constructor for the file
+  //Needs to add files to the directory vector
+}
+
+void printDirectory(vector<File>& newMyFile){
+  //Needs to loop through the directory of files and outputs the information
+}
